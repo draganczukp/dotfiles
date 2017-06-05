62ceebbca165d4674c3042d06e80953cffcaa217
Przemek Dagańczuk <mail@draganczuk.tk>
2017-06-05 16:12:42 +0000

    Now we tell you your browser sucks in your native tongue.

--- .config/polybar/mpris.sh
+++ .config/polybar/mpris.sh
@@ -2,7 +2,7 @@
 
 # Specifying the icon(s) in the script
 # This allows us to change its appearance conditionally
-icon=""
+icon=""
 
 player_status=$(playerctl status 2> /dev/null)
 if [[ $? -eq 0 ]]; then
@@ -11,7 +11,7 @@ fi
 
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
-    echo "%{F#D08770}$icon $metadata"       # Orange when playing
+    echo "%{F#AAC267}$icon $metadata"       # Orange when playing
 elif [[ $player_status = "Paused" ]]; then
     echo "%{F#65737E}$icon $metadata"       # Greyed out info when paused
 else
