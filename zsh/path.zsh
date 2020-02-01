8ee095abaaac09b08d902efff1e23640f3eaa36e
Przemek Dragańczuk <draganczukp@gmail.com>
2020-02-01 23:47:53 +0100

    Upgrade ruby

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,7 +2,7 @@ local _path=(
 	$HOME/bin
 	$HOME/.yarn/bin
 	$HOME/.config/yarn/global/node_modules/.bin
-	$HOME/.gem/ruby/2.6.0/bin
+	$HOME/.gem/ruby/2.7.0/bin
 	$HOME/.local/share/nvim/plugged/vim-superman/bin
 	/var/lib/snapd/snap/bin
 )
